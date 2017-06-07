5e71acb3f68ef469ed62975b6b4da16a64e301a9
omolewa <akinyomiomolewa30gmail.com>
2017-06-07 22:08:54 +0100

    edited the code

--- weather/weather.js
+++ weather/weather.js
@@ -1,3 +1,21 @@
-var request = require("request");
 var program = require ("commander");
-var url = http://samples.openweathermap.org/data/2.5/weather?q=Lagos,%20Nigeria&appid=b1b15e88fa797225412429c1c50c122a1
+var Weather = require("openweathermap")
+var weather = {
+	service: "openweathermap",
+	key: "b1b15e88fa797225412429c1c50c122a1",
+	units: "celcius",
+	cache: true,
+	ttl: {
+		minutes: 27,
+		seconds: 45
+	}
+}
+   weather.get([6.6080, 3.6218], function(err,weather) {
+	if (err) return console.dir(err);
+	console.dir(weather);
+})
+
+     weather.get([6.6080, 3.6218], true, function(err, weather){
+	if (err) return console.dir(err);
+	console.dir(weather);
+})
