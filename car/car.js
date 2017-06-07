c55c482e390c51d9269070abc7737671aee8c7c4
omolewa <akinyomiomolewa30gmail.com>
2017-06-07 11:00:23 +0100

    added files to lab1

---
+++ car/car.js
@@ -0,0 +1,42 @@
+class Car{
+	constructor(name, model, type){
+	if (name === undefined){
+	this.name ="General";
+	}
+	else{
+		this.name = name;
+	}
+	if (model === undefined){
+	this.model = "GM";
+	}
+	else{
+		this.model = model;
+	}
+    if (name === "Porshe" || name === "Koenigsegg"){
+    	this.numOfDoors = 2;
+    }
+    else{
+    	this.numOfDoors = 4;
+    }
+    if (type === "trailer"){
+    	this.numOfWheels = 8;
+    	this.isSaloon = false;
+    	this.speed = "0km/h";
+    	this.type = "trailer";
+    }
+    else{
+    	this.numOfWheels = 4;
+    	this.isSaloon= true;
+    }
+}
+    drive(gear){
+    	if (this.type === "trailer"){
+    		this.speed = 11 *gear + " " + "km/h";
+    	}
+    	else{
+    		this.speed = 50 * gear + " " + "km/hr";
+    	}
+    	return this;
+    }
+}
+module.exports = Car;
