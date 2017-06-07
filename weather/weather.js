9223f2882ecb394786b75a29de28ff59053a6b72
omolewa <akinyomiomolewa30gmail.com>
2017-06-07 10:57:48 +0100

    added files

---
+++ weather/weather.js
@@ -0,0 +1,3 @@
+var request = require("request");
+var program = require ("commander");
+var url = http://samples.openweathermap.org/data/2.5/weather?q=Lagos,%20Nigeria&appid=b1b15e88fa797225412429c1c50c122a1
